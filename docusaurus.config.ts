97d2f49f62cbd115572b42ef7878adee46077530
KoffeeKat <nksv.postbox@gmail.com>
2026-04-06 02:22:31 +0500

    chore: adjust config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://koffeekat.github.io",
+  url: "https://k0ffeekat.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
